327a4b7c6ff94bf968020b95b00d55303aa0a66e
Arnaud Gouel <gouel.arnaud@gmail.com>
2025-11-12 22:46:14 +0100

    feat: integrate DomPDF for PDF generation of recipes and add PDF download button in recipe view

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Recipe;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -25,5 +27,43 @@ final class RecipeController extends AbstractController
         return $this->render('recipe/show.html.twig', [
             'recipe' => $recipe
         ]);
+    }
+
+    #[Route('/{id}/pdf', name: 'pdf')]
+    public function pdf(Recipe $recipe): Response
+    {
+        // Configuration de DomPDF
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+        $pdfOptions->set('isRemoteEnabled', true);
+        $pdfOptions->set('isHtml5ParserEnabled', true);
+
+        // Instancier DomPDF
+        $dompdf = new Dompdf($pdfOptions);
+
+        // Générer le HTML depuis le template Twig
+        $html = $this->renderView('recipe/pdf.html.twig', [
+            'recipe' => $recipe
+        ]);
+
+        // Charger le HTML dans DomPDF
+        $dompdf->loadHtml($html);
+
+        // Rendre le PDF
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+
+        // Générer le nom du fichier
+        $filename = 'recette-' . preg_replace('/[^a-z0-9]+/', '-', strtolower($recipe->getTitle())) . '.pdf';
+
+        // Retourner la réponse PDF
+        return new Response(
+            $dompdf->output(),
+            Response::HTTP_OK,
+            [
+                'Content-Type' => 'application/pdf',
+                'Content-Disposition' => 'attachment; filename="' . $filename . '"',
+            ]
+        );
     }
 }
